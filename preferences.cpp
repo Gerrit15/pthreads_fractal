94193d34271522f7f16abb6ad3c764434843497a
Gerrit Roessler <roessg2@rpi.edu>
2026-02-03 23:03:13 -0500

    let's calm down

--- preferences.cpp
+++ preferences.cpp
@@ -16,7 +16,7 @@ int load_prefs(preferences &pref, int argc, char* argv[]) {
 	//setting defaults
 	pref.width = 30*32;
 	pref.height = 23*32;
-	pref.max_iteration = 1000;
+	pref.max_iteration = 100;
 	pref.max_threads = 5;
 	pref.max_mod = 2;
 	pref.outfile = "out.ppm";
