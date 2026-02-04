fd224766067e4bf68a6959802dc11d7b980d1957
Gerrit Roessler <roessg2@rpi.edu>
2026-02-03 22:30:09 -0500

    don't need this anymore

--- plane.h
+++ plane.h
@@ -9,19 +9,6 @@
 
 #include "preferences.h"
 
-//width and height are just the dimensions of the output iamge
-//30:23 is the ratio x:y
-#define SCALE 64
-#define WIDTH 30*SCALE
-#define HEIGHT 23*SCALE
-//mod as in modulo, magnitude of a complex numeber
-#define MAX_MOD 2
-//how many iterations can you try before saying "good enough"
-#define ITR_MAX 1000
-//how many threads do we make for this application
-#define THREAD_MAX 3
-
-
 typedef std::complex<double> cplx_num;
 static const cplx_num complex_const = 1; //complex constant for z = z^2+c
 
