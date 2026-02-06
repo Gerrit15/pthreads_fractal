969bb4e1df8a596f1e3ad57fda7fb5f09a2b5bc2
Gerrit Roessler <roessg2@rpi.edu>
2026-02-05 21:51:16 -0500

    benchmarking

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "plane.h"
+//#include <ctime>
 
 int main(int argc, char* argv[]) {
 	std::string outpath = "out.ppm";
+	//std::clock_t times[3];
+	//times[0] = clock();
 	plane complex_grid(argc, argv);
 	complex_grid.simulate();
+	//times[1] = clock();
 	complex_grid.output();
+	//times[2] = clock();
+	//std::cout << "Time to simulate: " << times[1] - times[0] << ", time to output: " << times[2] - times[1] << std::endl;
 }
